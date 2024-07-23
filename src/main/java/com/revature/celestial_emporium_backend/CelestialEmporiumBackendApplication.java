c5de6bfa0c27fd1560506f7dca4d387922306b38
Alonzo Sanchez <alonzosanchez@Alonzos-MacBook-Pro-2.local>
2024-07-23 16:12:01 -0500

    initial commit

---
+++ src/main/java/com/revature/celestial_emporium_backend/CelestialEmporiumBackendApplication.java
@@ -0,0 +1,13 @@
+package com.revature.celestial_emporium_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelestialEmporiumBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelestialEmporiumBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/celestial_emporium_backend/CelestialEmporiumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.celestial_emporium_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelestialEmporiumBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
