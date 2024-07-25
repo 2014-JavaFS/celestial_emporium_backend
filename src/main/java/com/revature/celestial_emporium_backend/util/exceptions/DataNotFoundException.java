c58461d5c96f545ae7b485ece8c5bd0e3cd35459
Elrey Bernal <elreyjcbernal@gmail.com>
2024-07-25 14:33:04 -0500

    Implemented ItemService and ItemController. Added exceptions such as DataNotFoundException and InvalidInputException

---
+++ src/main/java/com/revature/celestial_emporium_backend/util/exceptions/DataNotFoundException.java
@@ -0,0 +1,5 @@
+package com.revature.celestial_emporium_backend.util.exceptions;
+
+public class DataNotFoundException extends RuntimeException{
+    public DataNotFoundException(String message) { super(message); }
+}
