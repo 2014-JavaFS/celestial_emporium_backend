c6f5e318ac6cab8a6ad6d52c0ed4e1183a45966a
KayoJules <ChaoticPhoenix777@gmail.com>
2024-08-09 01:49:57 -0500

    Cart creation experimental

--- src/main/java/com/revature/celestial_emporium_backend/cartitem/CartItemService.java
+++ src/main/java/com/revature/celestial_emporium_backend/cartitem/CartItemService.java
@@ -1,17 +1,29 @@
 package com.revature.celestial_emporium_backend.cartitem;
+import com.revature.celestial_emporium_backend.cart.CartRepository;
+import com.revature.celestial_emporium_backend.users.UserRepository;
 import com.revature.celestial_emporium_backend.util.exceptions.DataNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
+import com.revature.celestial_emporium_backend.cart.Cart;
+import com.revature.celestial_emporium_backend.Inventory.InventoryRepository;
 
 @Service
 public class CartItemService {
     private final CartItemRepository cartItemRepository;
+    private final CartRepository cartRepository;
 
     @Autowired
-    public CartItemService(CartItemRepository cartItemRepository) { this.cartItemRepository = cartItemRepository; }
+    public CartItemService(CartItemRepository cartItemRepository, CartRepository cartRepository, UserRepository userRepository) {
+        this.cartItemRepository = cartItemRepository;
+        this.cartRepository = cartRepository;
+    }
+
+    @Autowired
+    private InventoryRepository inventoryRepository;
+
 
     public List<CartItem> findAll() {
         List<CartItem> cartItems = cartItemRepository.findAll();
@@ -24,8 +36,6 @@ public class CartItemService {
 
     public Optional<CartItem> findById(int id) { return cartItemRepository.findById(id); }
     public Optional<CartItem> findByUser(int id) { return cartItemRepository.findByUser(id); }
-
-    public CartItem create(CartItem cartItem) { return cartItemRepository.save(cartItem); }
 
     @Transactional
     public boolean delete(int cartItemId) {
@@ -41,6 +51,26 @@ public class CartItemService {
 
     public void checkout(int userId) {
         cartItemRepository.deleteByUserId(userId);
+    }
+
+    @Transactional
+    public CartItem create(CartItem cartItem) {
+        int userId = cartItem.getUser().getUserIdNumber();
+
+        Optional<Cart> existingCart = cartRepository.findByUserId(userId);
+
+        Cart cart;
+        if (existingCart.isEmpty()) {
+
+            cart = new Cart();
+            cart.setUser(cartItem.getUser());
+            cart = cartRepository.save(cart);
+        } else {
+            cart = existingCart.get();
+        }
+        cartItem.setCart(cart);
+
+        return cartItemRepository.save(cartItem);
     }
 
 }
